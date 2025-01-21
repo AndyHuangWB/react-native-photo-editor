4a90a8f374a3fa1483a0f2b9a09493eecd261eba
andy <andy.huang@wonderbricks.com>
2025-01-21 12:44:36 +0800

    chore: combine js to ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,51 @@
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native"
 
 export type Options = {
-  path: string;
-  stickers?: Array<string>;
-  animated: boolean;
-  photoQuality: number;
-};
+    path: string
+    stickers?: Array<string>
+    animated: boolean
+    photoQuality: number
+}
 
 export type ErrorCode =
-  | 'USER_CANCELLED'
-  | 'IMAGE_LOAD_FAILED'
-  | 'ACTIVITY_DOES_NOT_EXIST'
-  | 'FAILED_TO_SAVE_IMAGE'
-  | 'DONT_FIND_IMAGE'
-  | 'ERROR_UNKNOW';
+    | "USER_CANCELLED"
+    | "IMAGE_LOAD_FAILED"
+    | "ACTIVITY_DOES_NOT_EXIST"
+    | "FAILED_TO_SAVE_IMAGE"
+    | "DONT_FIND_IMAGE"
+    | "ERROR_UNKNOW"
+
+const { PhotoEditor } = NativeModules
+
+const defaultOptions: Options = {
+    path: "",
+    stickers: [],
+    animated: true,
+    photoQuality: 0.4,
+}
+
+const exportObject: PhotoEditorType = {
+    open: (optionsEditor: Partial<Options>): Promise<string> => {
+        const options = {
+            ...defaultOptions,
+            ...optionsEditor,
+        }
+
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response = await PhotoEditor.open(options)
+                if (response) {
+                    resolve(response)
+                }
+            } catch (e) {
+                reject(e)
+            }
+        })
+    },
+}
 
 type PhotoEditorType = {
-  open(option: Options): Promise<string>;
-};
+    open(option: Partial<Options>): Promise<string>
+}
 
-const { PhotoEditor } = NativeModules;
-
-export default PhotoEditor as PhotoEditorType;
+export default exportObject
